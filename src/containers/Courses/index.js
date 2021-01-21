5160d5d919b822a38fe61fc9cf422f54f44fa199
JohnB <jdbourland@college.harvard.edu>
2021-01-21 11:37:32 -0500

    Removed the old TODO Folder and removed it from App.js

--- src/containers/Courses/index.js
+++ src/containers/Courses/index.js
@@ -1,52 +1,57 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import {
   FormTitle, FormContainer, FormLabel, Select, Input, SubmitButton, SubmitButtonContent, ButtonLogo,
 } from './styles'
 import { ADD_COURSE } from './graphql'
 
-const CourseForm = () => (
+const CourseForm = () => {
+  const history = useHistory()
+  if (!localStorage.getItem('token')) {
+    history.push('/Login')
+  }
+  return (
+    <>
+      <FormTitle>New Course</FormTitle>
+      <form>
 
-  <>
-    <FormTitle>New Course</FormTitle>
-    <form>
+        <FormLabel htmlFor="name">Course Name</FormLabel>
+        <br />
+        <Input type="text" name="name" required />
 
-      <FormLabel htmlFor="name">Course Name</FormLabel>
-      <br />
-      <Input type="text" name="name" required />
+        <br />
+        <FormLabel htmlFor="days">Course Days</FormLabel>
+        <br />
+        <Select name="days" multiple required>
+          <option value="mon">Monday</option>
+          <option value="tues">Tuesday</option>
+          <option value="wed">Wednesday</option>
+          <option value="thurs">Thursday</option>
+          <option value="fri">Friday</option>
+          <option value="sat">Saturday</option>
+          <option value="sun">Sunday</option>
+        </Select>
 
-      <br />
-      <FormLabel htmlFor="days">Course Days</FormLabel>
-      <br />
-      <Select name="days" multiple required>
-        <option value="mon">Monday</option>
-        <option value="tues">Tuesday</option>
-        <option value="wed">Wednesday</option>
-        <option value="thurs">Thursday</option>
-        <option value="fri">Friday</option>
-        <option value="sat">Saturday</option>
-        <option value="sun">Sunday</option>
-      </Select>
-
-      <br />
-      <FormLabel htmlFor="start">Course Times</FormLabel>
-      <br />
-      <Input type="time" name="start" min="00:00" max="21:59" required placeholder="Start Time" />
+        <br />
+        <FormLabel htmlFor="start">Course Times</FormLabel>
+        <br />
+        <Input type="time" name="start" min="00:00" max="21:59" required placeholder="Start Time" />
                 &nbsp;&nbsp;&nbsp; to &nbsp;&nbsp;&nbsp;
-      <Input type="time" name="end" min="00:00" max="21:59" required placeholder="End Time" />
+        <Input type="time" name="end" min="00:00" max="21:59" required placeholder="End Time" />
 
-      <br />
-      <SubmitButton className="bounce-button">
-        <SubmitButtonContent>
-          <ButtonLogo><span uk-icon="plus" ratio=".7" /></ButtonLogo>
+        <br />
+        <SubmitButton className="bounce-button">
+          <SubmitButtonContent>
+            <ButtonLogo><span uk-icon="plus" ratio=".7" /></ButtonLogo>
         Submit
-        </SubmitButtonContent>
-      </SubmitButton>
+          </SubmitButtonContent>
+        </SubmitButton>
 
-    </form>
-  </>
+      </form>
+    </>
 
-)
-
+  )
+}
 const CourseList = () => (
 
   <div>List</div>
