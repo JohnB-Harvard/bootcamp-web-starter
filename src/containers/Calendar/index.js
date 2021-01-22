e1dc7eef3f5bfbfc42c215546d25c9bd1ec3ddfe
bellatarantino <bellatarantino@college.harvard.edu>
2021-01-22 10:09:06 -0500

    Calendar styling

--- src/containers/Calendar/index.js
+++ src/containers/Calendar/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { useGlobalContext } from '../../utils/GlobalContext'
+import { Container, Title, Button, ButtonContent, ButtonLogo, ButtonLink } from './styles'
 
 const Calendar = () => {
   const globalState = useGlobalContext()
@@ -9,10 +10,18 @@ const Calendar = () => {
     history.push('/Login')
   }
   return (
-    <>
-      <p>Calendar</p>
-      <a href="https://calendar.google.com/calendar/u/0/r" target="_">View your calendar</a>
-    </>
+    <Container>
+      <Title>Your Google Calendar</Title>
+      
+
+      <Button>
+      <ButtonContent>
+        <ButtonLogo><span uk-icon="calendar" ratio=".7" /></ButtonLogo>
+        <ButtonLink href="https://calendar.google.com/calendar/u/0/r" target="_">View your calendar</ButtonLink>
+      </ButtonContent>
+    </Button>
+    </Container>
+
   )
 }
 
